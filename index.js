7815e0ff3c1a738b60b01997df219d6eea4cad60
pascal19821003 <pascal1982@sina.com>
2021-08-16 16:07:15 +0800

    aa

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = require('./lib/HelloWorld.js')
+
+// module.exports = require('./lib/HelloWorld.js')
+import { hello,max } from "./src/HelloWorld";
+import { greeter } from "./src/greeter";
+export {
+    hello,max,greeter
+}
 
 
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,15 +1,33 @@
 /** 测试类 */
-class HelloWorld {
+// class HelloWorld {
 
-    /** 
-     * 在页面中显示hello world dddd
-     */
-    say() {
-        let div = document.createElement('div')
+//     /** 
+//      * 在页面中显示hello world dddd
+//      */
+//     say() {
+//         let div = document.createElement('div')
 
-        div.innerHTML = 'hello world'
+//         div.innerHTML = 'hello world'
 
-        document.body.appendChild(div)
+//         document.body.appendChild(div)
 
+//     }
+
+//     myDo(){
+//         return "hahaha"
+//     }
+// }
+// let hw = new HelloWorld()
+// console.log("hw", hw.myDo())
+
+export function hello(){
+    return "hello world!"
+}
+
+export function max(num1, numb2){
+    if(num1>numb2){
+        return num1
+    }else{
+        return numb2
     }
 }
